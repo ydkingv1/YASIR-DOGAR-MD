dfec5e5e7de40dc8752f276bcbf16d3bde4387dd
YASIR DOGAR <108791600+ydkingv1@users.noreply.github.com>
2022-07-30 10:44:24 +0500

    Update truecaller.js
    Signed-off-by: YASIR DOGAR <108791600+ydkingv1@users.noreply.github.com>

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -1,7 +1,7 @@
 /* Copyright (C) 2022 Sourav KL11.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Raganork MD - Sourav KL11
+YD KING MD - Yasir dogar KL11
 */
 const {find} = require('raganork-bot')
 const {Module} = require('../main');
@@ -9,7 +9,7 @@ const s = require('../config');
 const wk = s.MODE == 'public' ? false : true
 Module({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',use: 'search',fromMe: wk}, async (msg, query) => {
 if (!query[1] && !msg.reply_message) return await msg.reply("_Give me any number or reply to any user!_");
-if (query[1].includes('/')) return await msg.sendReply('Wrong format! \n\n .true +91 6282344739')
+if (query[1].includes('/')) return await msg.sendReply('Wrong format! \n\n .true +92 3002611878')
 var go;
 if (msg.reply_message) go = msg.reply_message.jid.split('@')[0]
 else if (!query[1].includes('@')) go = query[1]
