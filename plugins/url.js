c58c287d83cffbe257ebf9f3fc598665e1183c2e
YASIR DOGAR <108791600+ydkingv1@users.noreply.github.com>
2022-07-30 10:48:28 +0500

    Update url.js
    Signed-off-by: YASIR DOGAR <108791600+ydkingv1@users.noreply.github.com>

--- plugins/url.js
+++ plugins/url.js
@@ -2,7 +2,7 @@
 /* Copyright (C) 2022 Sourav KL11.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Raganork MD - Sourav KL11
+YD KING MD - Yasir dogar KL11
 */ 
 const {MODE,IMGBB_KEY} = require('../config');
 async function webpUpload(file){
